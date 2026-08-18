928cd8597af7404dd63325bb61d478000a40d510
xyron11 <xxyron604@gmail.com>
2026-08-18 17:50:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const API = "https://danzclean-api.arisu.biz.id";
+const API = "http://danz-api.arisu.biz.id:10496";
 const DOWNLOAD_API = "https://api-download-beryl.vercel.app";
 const YT_API = "https://api.azbry.com/api/download";
