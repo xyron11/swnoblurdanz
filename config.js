0715135596419cbcb5185efa0fc676c91cc1f4ee
xyron11 <xxyron604@gmail.com>
2026-08-23 13:54:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const API = "https://danz-api.arisu.biz.id:10497";
 const DOWNLOAD_API = "https://api-download-beryl.vercel.app";
 const YT_API = "https://api.azbry.com/api/download";
-const EMAIL_API = "https://inquisitive-platypus-c3bfdc.netlify.app/.netlify/functions/send-email";
+const EMAIL_API = "https://snsndkskkd-production.up.railway.app/send-email";
 
 const CS_API = "https://gmmitu-production.up.railway.app/api/send-gemini";
 
